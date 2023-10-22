a16d0080502ede13aed492e672dbff5f1327c00a
Gleb Klyga <klyga90@bk.ru>
2023-10-22 20:51:29 +0300

    Initial Commit

---
+++ Finance_Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Finance_Calculator
+//
+//  Created by Глеб Клыга on 22.10.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
